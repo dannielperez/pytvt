ce00a0ebc307be092b7554fed563cd6658a7191b
Danniel Perez <dannielperezvelez@gmail.com>
2026-04-06 17:34:09 -0400

    feat: export public API from package root
    Export NvrClient, Channel, RtspServerConfig, ApiServerConfig, PortConfig,
    and NvrApiError from pytvt.__init__ for convenient top-level imports.

--- src/pytvt/__init__.py
+++ src/pytvt/__init__.py
@@ -4,6 +4,7 @@ Public API:
     Constants: BackendFamily, IntegrationMode, CompositeStrategy, ExecutionPlan,
                resolve_backend, resolve_execution_plan
     Models:    ScannerConfig, DeviceEntry, CameraInfo, ScanResult, NvrApiError
+    NVR API:   NvrClient, Channel, RtspServerConfig, ApiServerConfig, PortConfig
     Config:    load_config
     Scanner:   scan_single_nvr, load_devices, filter_tvt_devices
     Output:    save_csv, save_json, save_xlsx_per_site, save_failed_devices
@@ -17,12 +18,17 @@ from .constants import BackendFamily, CompositeStrategy, ExecutionPlan, Integrat
 from .diff import DeviceDiff, ScanDiff, diff_scans, load_scan_file
 from .exceptions import BackendError, PytvtError, RegistryError
 from .models import (
+    ApiServerConfig,
     CameraInfo,
+    Channel,
     DeviceEntry,
     NvrApiError,
+    PortConfig,
+    RtspServerConfig,
     ScannerConfig,
     ScanResult,
 )
+from .nvr_api import NvrClient
 from .output import save_csv, save_failed_devices, save_json, save_xlsx_per_site
 from .registry import resolve_execution_plan
 from .scanner import filter_tvt_devices, load_devices, scan_single_nvr
@@ -42,11 +48,17 @@ __all__ = [
     "PytvtError",
     "RegistryError",
     # Models
+    "ApiServerConfig",
     "CameraInfo",
+    "Channel",
     "DeviceEntry",
     "NvrApiError",
+    "PortConfig",
+    "RtspServerConfig",
     "ScannerConfig",
     "ScanResult",
+    # NVR API client
+    "NvrClient",
     # Config
     "load_config",
     # Scanner
